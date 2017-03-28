7e75b2711dc189e6a7c1ce66af0456dbc01927ee
kasem <kasem.w@dzcard.com>
2017-03-28 13:03:14 +0700

    change param from values to video of post method

--- MyTheatre.Api/Controllers/VideoController.cs
+++ MyTheatre.Api/Controllers/VideoController.cs
@@ -11,40 +11,40 @@ namespace MyTheatreApi.Controllers
     [Route("api/[controller]")]
     public class VideoController : Controller
     {
-        private VideoContext  db;
+        private VideoContext  _db;
         public VideoController()
         {
-            db = new VideoContext();
+            _db = new VideoContext();
         }
-        // GET api/values
+        // GET api/Video
         [HttpGet]
         public IEnumerable<Video> Get()
         {
-            return db.Videos;
+            return _db.Videos;
         }
 
-        // GET api/values/5
+        // GET api/Video/5
         [HttpGet("{id:int}")]
         public Video Get(int id)
         {
-            return db.Videos.FirstOrDefault(v=> v.Id == id);
+            return _db.Videos.FirstOrDefault(v=> v.Id == id);
         }
 
-        // POST api/values
+        // POST api/Video
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Video video)
         {
-             
+             _db.Videos.Add(video);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        // PUT api/Video/5
+        [HttpPut("{id:int}")]
+        public void Put(int id, [FromBody]Video video)
         {
             
         }
 
-        // DELETE api/values/5
+        // DELETE api/video/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
