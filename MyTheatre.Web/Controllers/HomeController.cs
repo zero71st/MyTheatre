3bd9a73701192a952ba559d8f96019f53c79f4b5
Kasem Wongsa <Kasem Wongsa>
2017-03-31 21:53:53 +0700

    Added display model to UI

--- MyTheatre.Web/Controllers/HomeController.cs
+++ MyTheatre.Web/Controllers/HomeController.cs
@@ -4,18 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
+using Newtonsoft.Json;
+using MyTheatre.Domain;
 
 namespace MyTheatre.Web.Controllers
 {
     public class HomeController : Controller
     {
         private HttpClient _apiClient;
-        private readonly string _remoteServiceBaseUrl = "http://192.168.99.100:5000/api/videos";
-        public IActionResult Index()
+        private readonly string _remoteServiceBaseUrl = "http://localhost:5000/api/videos";
+        public async Task<IActionResult> Index()
         {
             _apiClient = new HttpClient();
-            var result = _apiClient.GetStringAsync(_remoteServiceBaseUrl);
-            return View(result);
+            var dataString =  await _apiClient.GetStringAsync(_remoteServiceBaseUrl);
+
+              var videos = JsonConvert.DeserializeObject<List<Video>>(dataString);
+
+            return View(videos);
         }
 
         public IActionResult About()
