1682ea74f8c2363365350cdc18dba574b46a41fa
kasem <kasem.w@dzcard.com>
2017-03-31 16:06:03 +0700

    update mvc home controller

--- MyTheatre.Web/Controllers/HomeController.cs
+++ MyTheatre.Web/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace MyTheatre.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private HttpClient _apiClient;
+        private readonly string _remoteServiceBaseUrl = "http://192.168.99.100:5000/api/videos";
         public IActionResult Index()
         {
-            return View();
+            _apiClient = new HttpClient();
+            var result = _apiClient.GetStringAsync(_remoteServiceBaseUrl);
+            return View(result);
         }
 
         public IActionResult About()
